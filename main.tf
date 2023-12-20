0d1dcf357b352122cc8d9b1d6304bd43d630dcf5
tomon9086 <tomon9086@gmail.com>
2023-12-20 10:00:22 +0900

    wip
    operation error Scheduler: CreateSchedule,
    ValidationException: The execution role you provide must allow AWS EventBridge Scheduler to assume the role.

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_s3_bucket_acl" "lambda_bucket" {
   acl    = "private"
 }
 
-data "archive_file" "lambda_hello_world" {
+data "archive_file" "lambda" {
   type        = "zip"
   source_dir  = "dist"
   output_path = "/tmp/lambda_function_payload.zip"
@@ -77,39 +77,46 @@ data "archive_file" "lambda_hello_world" {
   # ]
 }
 
-resource "aws_s3_object" "lambda_hello_world" {
+resource "aws_s3_object" "lambda" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
   key    = "hello-world.zip"
-  source = data.archive_file.lambda_hello_world.output_path
+  source = data.archive_file.lambda.output_path
 
-  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+  etag = filemd5(data.archive_file.lambda.output_path)
 
   tags = {
     project = local.project_code
   }
 }
 
-resource "aws_lambda_function" "hello_world" {
-  function_name = "HelloWorld"
+resource "random_pet" "lambda_test-http_name" {
+  prefix = local.project_code
+  length = 4
+}
+
+resource "aws_lambda_function" "test-http" {
+  function_name = random_pet.lambda_test-http_name.id
 
   s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_hello_world.key
+  s3_key    = aws_s3_object.lambda.key
 
   runtime = "nodejs20.x"
   handler = "index.handler"
 
-  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   role = aws_iam_role.lambda_exec.arn
 
   tags = {
     project = local.project_code
   }
+
+  depends_on = [aws_s3_object.lambda]
 }
 
-resource "aws_cloudwatch_log_group" "hello_world" {
-  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+resource "aws_cloudwatch_log_group" "test-http" {
+  name = "/aws/lambda/${aws_lambda_function.test-http.function_name}"
 
   retention_in_days = 30
 
@@ -182,19 +189,19 @@ resource "aws_apigatewayv2_stage" "lambda" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "hello_world" {
+resource "aws_apigatewayv2_integration" "test-http" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+  integration_uri    = aws_lambda_function.test-http.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
 
-resource "aws_apigatewayv2_route" "hello_world" {
+resource "aws_apigatewayv2_route" "test-http" {
   api_id = aws_apigatewayv2_api.lambda.id
 
   route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.test-http.id}"
 }
 
 resource "aws_cloudwatch_log_group" "api_gw" {
@@ -210,7 +217,7 @@ resource "aws_cloudwatch_log_group" "api_gw" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
+  function_name = aws_lambda_function.test-http.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
@@ -219,3 +226,102 @@ resource "aws_lambda_permission" "api_gw" {
 output "base_url" {
   value = "${aws_apigatewayv2_stage.lambda.invoke_url}/hello"
 }
+
+resource "random_pet" "lambda_test-cron_name" {
+  prefix = local.resource_prefix
+  length = 4
+}
+
+resource "aws_lambda_function" "test-cron" {
+  function_name = random_pet.lambda_test-cron_name.id
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda.key
+
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+
+  tags = {
+    project = local.project_code
+  }
+
+  depends_on = [aws_s3_object.lambda]
+}
+
+resource "random_pet" "scheduler_name" {
+  prefix = local.resource_prefix
+  length = 4
+}
+
+data "aws_iam_policy_document" "scheduler" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+      "scheduler:CreateSchedule",
+      "scheduler:DeleteSchedule",
+      "scheduler:GetSchedule",
+      "scheduler:UpdateSchedule",
+    ]
+    resources = [
+      "arn:aws:events:::*",
+      "arn:aws:schedule:::*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "scheduler" {
+  name        = "scheduler-policy"
+  description = "A scheduler policy"
+
+  policy = data.aws_iam_policy_document.scheduler.json
+}
+
+resource "aws_iam_role" "scheduler" {
+  name = "lambda_cron_scheduler"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service : "scheduler.amazonaws.com"
+      }
+      }
+    ]
+  })
+
+  tags = {
+    project = local.project_code
+  }
+}
+
+resource "aws_iam_policy_attachment" "scheduler_policy" {
+  name       = "scheduler-attachment"
+  roles      = ["${aws_iam_role.scheduler.name}"]
+  policy_arn = aws_iam_policy.scheduler.arn
+}
+
+
+resource "aws_scheduler_schedule" "test-cron" {
+  name       = random_pet.scheduler_name.id
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression          = "cron(*/10 * * * ? *)"
+  schedule_expression_timezone = "Asia/Tokyo"
+
+  target {
+    arn      = aws_lambda_function.test-cron.arn
+    role_arn = aws_iam_role.lambda_exec.arn
+  }
+}
+
